eabf193bb60de66222c6cf3d355ee7d399d06370
NooraF1234 <122235174+NooraF1234@users.noreply.github.com>
2024-11-02 22:10:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return send_from_directory('', 'index.html')
+
+@app.route('/Eye_tracking_data/<path:filename>')
+def send_eye_tracking_data(filename):
+    return send_from_directory('Eye_tracking_data', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
